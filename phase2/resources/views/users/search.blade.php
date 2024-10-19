6b5db6b36eaebfda34b891a5ccaab650d97dd3a5
kenta-kenta <kenken22352235@gmail.com>
2024-10-19 11:54:40 +0900

    ライトモードの調整をしました

--- phase2/resources/views/users/search.blade.php
+++ phase2/resources/views/users/search.blade.php
@@ -23,11 +23,12 @@
                 {{ $users->appends(request()->input())->links() }}
             </div>
             @foreach ($users as $user)
-            <div class="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
-                <input type="button" name="select" class="mr-2" value="選択">
+            <div class="mb-4 p-4 bg-gray-200 dark:bg-gray-700 rounded-lg">
+                <input type="checkbox" name="select" class="mr-2" value="選択">
                 <p class="text-gray-800 dark:text-gray-300">{{ $user->account_name }}</p>
             </div>
             @endforeach
+            <button class="ml-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">決定する</button>
             @else
             <div>
                 <p class="text-gray-800 dark:text-gray-300">ユーザーが見つかりませんでした</p>
