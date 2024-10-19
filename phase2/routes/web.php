e3277c164ee0d24d1a3cdb8acaba9c1f623fb1ed
kenta-kenta <kenken22352235@gmail.com>
2024-10-19 11:36:42 +0900

    laravelのコンポーネントを追加しました

--- phase2/routes/web.php
+++ phase2/routes/web.php
@@ -8,7 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/search', [UserController::class, 'search'])->name('users.search');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,6 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/search', [UserController::class, 'search'])->name('users.search');
 });
 
 require __DIR__ . '/auth.php';
