0369838ea295e3073d06f180ee33e6affc481958
kenta-kenta <kenken22352235@gmail.com>
2024-10-20 13:32:45 +0900

    フォロー欄の改善

--- phase2/resources/views/profile/followers.blade.php
+++ phase2/resources/views/profile/followers.blade.php
@@ -1,9 +1,11 @@
 <!-- resources/views/user/followers.blade.php -->
-
-<h3>{{ Auth::user()->account_name }} のフォロワー</h3>
-<ul>
-    @foreach ($followers as $follower)
-        <li>{{ $follower->account_name }}</li>
-    @endforeach
-</ul>
-
+<x-app-layout>
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <h3 class="dark:text-white">{{ Auth::user()->account_name }} のフォロワー</h3>
+        <ul>
+            @foreach ($followers as $follower)
+            <li class="dark:text-white">{{ $follower->account_name }}</li>
+            @endforeach
+        </ul>
+    </div>
+</x-app-layout>
--- phase2/resources/views/profile/followings.blade.php
+++ phase2/resources/views/profile/followings.blade.php
@@ -1,9 +1,11 @@
 <!-- resources/views/user/followings.blade.php -->
-
-<h3>{{ Auth::user()->account_name }} がフォローしているユーザー</h3>
-<ul>
-    @foreach ($followings as $following)
-        <li>{{ $following->account_name }}</li>
-    @endforeach
-</ul>
-
+<x-app-layout>
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+        <h3 class="dark:text-white">{{ Auth::user()->account_name }} がフォローしているユーザー</h3>
+        <ul>
+            @foreach ($followings as $following)
+            <li class="dark:text-white">{{ $following->account_name }}</li>
+            @endforeach
+        </ul>
+    </div>
+</x-app-layout>
